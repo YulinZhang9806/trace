fc6b06c16c7c9d6c5f0b70f41df5f84841dfb58f
Arjun Biddanda <aabiddanda@gmail.com>
2025-01-26 08:43:50 -0500

    draft: some small restructuring

---
+++ ghosthmm/cli.py
@@ -0,0 +1,40 @@
+"""CLI for ghosthmm."""
+import logging
+import sys
+
+import click
+import numpy as np
+import pandas as pd
+
+from Ghosthmm import GhostHMM
+
+# Setup the logging configuration for the CLI
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+@click.command()
+@click.option(
+    "--input",
+    "-i",
+    required=True,
+    type=click.Path(exists=True),
+    help="Input data file for PGT-A array intensity data.",
+)
+@click.option(
+    "--out",
+    "-o",
+    required=True,
+    type=str,
+    default="karyohmm",
+    help="Output file prefix.",
+)
+def main(
+    input,
+    out="ghosthmm",
+):
+    """GhostHMM-Inference CLI."""
+    logging.info(f"Starting to read input data {input}...")
